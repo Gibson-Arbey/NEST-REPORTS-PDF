87a7250fcffb6651b0ef48062246da0a840962f7
Gibson-Arbey <gibsonarbey@gmail.com>
2025-03-23 18:06:48 -0500

    se crea reportes de graficas, y se crea funcionalidad para convertir html a pdf con pdfmake

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { BasicReportsModule } from './basic-reports/basic-reports.module';
 import { PrinterModule } from './printer/printer.module';
 import { ReportPuppeteerModule } from './report-puppeteer/report-puppeteer.module';
 import { StoreReportsModule } from './store-reports/store-reports.module';
+import { ExtraReportsModule } from './extra-reports/extra-reports.module';
 
 @Module({
-  imports: [BasicReportsModule, PrinterModule, ReportPuppeteerModule, StoreReportsModule],
+  imports: [BasicReportsModule, PrinterModule, ReportPuppeteerModule, StoreReportsModule, ExtraReportsModule],
   controllers: [],
   providers: [],
 })
--- src/store-reports/store-reports.service.ts
+++ src/store-reports/store-reports.service.ts
@@ -1,44 +1,79 @@
 import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrinterService } from 'src/printer/printer.service';
+import { getBasicChartSvgReport } from 'src/reports/basic-chart-svg.report';
 import { orderByIdReport } from 'src/reports/order-by-id.report';
+import { getStatisticsReport } from 'src/reports/statistics.report';
 
 @Injectable()
 export class StoreReportsService extends PrismaClient implements OnModuleInit {
-    async onModuleInit() {
-      await this.$connect();
-    }
-  
-    constructor(private readonly printerService: PrinterService) {
-      super();
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  constructor(private readonly printerService: PrinterService) {
+    super();
+  }
+
+  async getOrderByIdReport(orderId: number) {
+    const order = await this.orders.findUnique({
+      where: {
+        order_id: orderId,
+      },
+      include: {
+        customers: true,
+        order_details: {
+          include: {
+            products: true,
+          },
+        },
+      },
+    });
+
+    if (!order) {
+      throw new NotFoundException(`La orden con id ${orderId} no existe`);
     }
 
+    const docDefinition = orderByIdReport({
+      data: order as any,
+    });
 
-    async getOrderByIdReport(orderId: number) {
-        const order = await this.orders.findUnique({
-            where: {
-              order_id: orderId,
-            },
-            include: {
-              customers: true,
-              order_details: {
-                include: {
-                  products: true,
-                },
-              },
-            },
-          });
-      
-          if (!order) {
-            throw new NotFoundException(`La orden con id ${orderId} no existe`);
-          }
-      
-          const docDefinition = orderByIdReport({
-            data: order as any,
-          });
-      
-          const doc = this.printerService.createPdf(docDefinition);
-      
-          return doc;
-    }
+    const doc = this.printerService.createPdf(docDefinition);
+
+    return doc;
+  }
+
+  async getSvgChart() {
+    const docDefinition = await getBasicChartSvgReport();
+
+    const doc = this.printerService.createPdf(docDefinition);
+
+    return doc;
+  }
+
+  async getStatistics() {
+    const topCountries = await this.customers.groupBy({
+      by: ['country'],
+      _count: true,
+      orderBy: {
+        _count: {
+          country: 'desc',
+        },
+      },
+      take: 10,
+    });
+
+    const topCountryData = topCountries.map(({ country, _count }) => ({
+      country: country ?? '',
+      customers: _count,
+    }));
+
+    const docDefinition = await getStatisticsReport({
+      topCountries: topCountryData,
+    });
+
+    const doc = this.printerService.createPdf(docDefinition);
+
+    return doc;
+  }
 }
